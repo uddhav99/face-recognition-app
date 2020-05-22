44770f2ca2d8d1e26a857a0a0e83f4b960bc841c
ubhagat <ubhagat@umich.edu>
2020-05-22 19:45:05 +0530

    working on database

--- server/server.js
+++ server/server.js
@@ -2,8 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const app = express();
+const knex = require('knex');
 
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'uddhavbhagat', 
+        password: '', 
+        database: 'smart-brain'
+    }
+})
+
+console.log(db.select('*').from('users'));
+
+const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -30,7 +43,6 @@ const database = {
 
 app.get('/', (req, res) => {
     res.send(database.users);
-
 })
 
 app.post('/signin', (req, res) => {
@@ -38,15 +50,47 @@ app.post('/signin', (req, res) => {
 })
 
 app.post('/register', (req, res) => {
-    
+    const { name, email, password } = req.body;
+    db('users')
+        .returning('*')
+        .insert({
+        name: name, 
+        email: email, 
+        joined: new Date()
+        })
+        .then(user => {
+            res.json(user[0]);
+        })
+        .catch(err => res.status(400).json('unable to register'))
+
 })
 
 app.get('/profile/:id', (req, res) => {
-
+    const { id } = req.params;
+    db.select('*').from('users').where({
+        id: id
+        })
+        .then(user => {
+            if (user.length) {
+                res.json(user[0]);
+            } else {
+                res.status(400).json('Not found');
+            }
+        })
+        .catch(err => res.status(400).json('error getting user'))
 })
 
 app.put('/image', (req, res) => {
-    
+    const { id } = req.body;
+    db('users')
+        .where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0]);
+        })
+        .catch(err => res.status(400).json('unable to get entries'));
+
 })
 
 app.listen(3000, () => {
