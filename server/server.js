ee35cb21f12a676e9c10500cb0e34e87dbb1260a
ubhagat <ubhagat@umich.edu>
2020-05-23 05:14:44 +0530

    adding port

--- server/server.js
+++ server/server.js
@@ -38,6 +38,6 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(3000, () => {
-    console.log('testing app');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on ${process.env.PORT}`);
 })
